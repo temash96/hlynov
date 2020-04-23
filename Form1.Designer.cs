93f61730e8392bdb4d84084cbac1a425b5184ab5
Артём Шаляпин <temash96@gmail.com>
2020-04-23 15:55:29 +0300

    version 2.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column7,
+            this.Column9});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "id";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Сумма";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Сроки";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Описание";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 125;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4,
+            this.Column8,
+            this.Column10});
+            this.dataGridView2.Location = new System.Drawing.Point(654, 46);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(612, 292);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Id";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Сумма";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Сроки";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Описание";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 125;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column6});
+            this.dataGridView3.Location = new System.Drawing.Point(95, 387);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(445, 310);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Пассив";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Актив";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(793, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(270, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Активы";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(936, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пассивы";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Helvetica", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(241, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Пересечения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1278, 711);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,420 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.SQLite;
 
 namespace fond
 {
+
     public partial class Form1 : Form
     {
+
+        int[,] x = { {1,0}, // заполняем пересечения
+{3,0},
+{7,0 },
+{11,0 },
+{3,1 },
+{8,1 },
+{11,1 },
+{2,2 },
+{4,2 },
+{7,2 },
+{11,2 },
+{5,3 },
+{8,3 },
+{11,3 },
+{8,4 },
+{9,4 },
+{11,4 },
+{13,4 },
+{6,5 },
+{9,5 },
+{11,5 },
+{13,5 },
+{6,6 },
+{9,6 },
+{11,6 },
+{13,6 },
+{5,7 },
+{10,7 },
+{11,7 },
+{1,8 },
+{2,8 },
+{3,8 },
+{8,8 },
+{11,8 },
+{1,9 },
+{2,9 },
+{3,9 },
+{0,10 },
+{2,10 },
+{13,11 },
+{15,11 },
+{16,11 },
+{7,12 },
+{7,13 },
+{11,13 },
+{12,13 },
+{15,14 },
+{18,14 },
+{4,15 },
+{5,15 },
+{6,15 },
+{11,15 },
+{13,16 },
+{14,16 },
+{11,17 },
+{15,17 },
+{17,17 },
+{1,18 },
+{2,18 }};
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+            Present(LoadDataActive(), LoadDataPassive());
+
+            int K = x.GetLength(0);
+            int L = x.GetLength(1);
+            //вывод стандартных пересечений
+            dataGridView3.RowCount = K;
+            dataGridView3.ColumnCount = L;
+            for (int i = 0; i < K; ++i)
+                for (int j = 0; j < L; ++j)
+                    dataGridView3.Rows[i].Cells[j].Value = x[i, j];
+
+        }
+
+        private List<string[]> LoadDataPassive()
+        { // Массив Пассивов из БД
+          // Подключение к БД
+          // string connetionString = "Data Source=localhost;Initial Catalog=bank;User ID=root;Password=559966092";
+          //MySqlConnection cnn = new MySqlConnection(connetionString);
+            string connetionString = @"Data Source=.\bank.db;Version=3;";
+            SQLiteConnection cnn = new SQLiteConnection(connetionString);
+            cnn.Open();
+
+            //вывод пассивов из базы данных в data_act
+            string query_pas = "SELECT * FROM passive ORDER BY id";
+
+            SQLiteCommand command_pas = new SQLiteCommand(query_pas, cnn);
+            SQLiteDataReader reader_pas = command_pas.ExecuteReader();
+            List<string[]> data_pas = new List<string[]>();
+
+            while (reader_pas.Read())
+            {
+                data_pas.Add(new string[4]);
+                data_pas[data_pas.Count - 1][0] = reader_pas[0].ToString();
+                data_pas[data_pas.Count - 1][1] = reader_pas[1].ToString();
+                data_pas[data_pas.Count - 1][2] = reader_pas[2].ToString();
+                data_pas[data_pas.Count - 1][3] = reader_pas[3].ToString();
+            }
+            reader_pas.Close();
+            cnn.Close();
+            return data_pas;
+
+        }
+
+        private List<string[]> LoadDataActive() // Массив Активов из БД
+        {
+            // Подключение к БД
+          //string connetionString = "Data Source=localhost;Initial Catalog=bank;User ID=root;Password=559966092";
+            string connetionString = @"Data Source=.\bank.db;Version=3;";
+            SQLiteConnection cnn = new SQLiteConnection(connetionString);
+            cnn.Open();
+
+            //вывод активов из базы данных в data_act
+            string query_act = "SELECT * FROM active ORDER BY id";
+            SQLiteCommand command_act = new SQLiteCommand(query_act, cnn);
+            SQLiteDataReader reader_act = command_act.ExecuteReader();
+            List<string[]> data_act = new List<string[]>();
+
+            while (reader_act.Read())
+            {
+                data_act.Add(new string[4]);
+                data_act[data_act.Count - 1][0] = reader_act[0].ToString();
+                data_act[data_act.Count - 1][1] = reader_act[1].ToString();
+                data_act[data_act.Count - 1][2] = reader_act[2].ToString();
+                data_act[data_act.Count - 1][3] = reader_act[3].ToString();
+            }
+            reader_act.Close();
+            cnn.Close();
+            return data_act;
+        }
+
+        // вывод в datagridwiew из таблицы БД
+        private void Present(in List<string[]> act, in List<string[]> pas)
+        {
+
+            foreach (string[] s in act)
+            {
+                dataGridView1.Rows.Add(s);
+            }
+            foreach (string[] s in pas)
+            {
+                dataGridView2.Rows.Add(s);
+            }
+        }
+
+        private int[,] intersection()
+        {
+            //int n = dataGridView3.RowCount;
+
+            //int i, j;
+            int[,] DataValue = new int[dataGridView3.Rows.Count, dataGridView3.Columns.Count];
+
+            foreach (DataGridViewRow row in dataGridView3.Rows)
+            {
+
+                foreach (DataGridViewColumn column in dataGridView3.Columns)
+                {
+                    DataValue[row.Index, column.Index] = Convert.ToInt32(dataGridView3.Rows[row.Index].Cells[column.Index].Value);
+                }
+            }
+            return DataValue;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] intersec = intersection();
+            List<string[]> act_str = LoadDataActive();
+            List<string[]> pas_str = LoadDataPassive();
+            //Конвертация в тип double
+            double[,] act = new double[act_str.Count, 2];
+            double[,] pas = new double[pas_str.Count, 2];
+
+            for (int i = 0; i < act_str.Count; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    act[i, j] = Convert.ToDouble(act_str[i][j + 1]);
+                }
+            }
+            for (int i = 0; i < pas_str.Count; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    pas[i, j] = Convert.ToDouble(pas_str[i][j + 1]);
+                }
+            }
+
+
+            int rezerv = 7;
+            int nom_rez = 11;
+
+            int kol_R = pas.GetLength(0) + act.GetLength(0) + rezerv;
+            int kol_S = rezerv;
+
+            double M = 10;
+            double[,] otvet = new double[kol_R, 2];
+
+            double sum_rez = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                sum_rez += act[i, 0];
+            }
+
+            //формируем симплекс таблицу
+            double[,] matr = new double[1 + kol_R, intersec.GetLength(0) + kol_S + kol_R + 1];
+
+            //заполняем z строку (целевая функция)
+            for (int i = 0; i < intersec.GetLength(0); i++)
+            {
+                matr[0, i] = -(pas[intersec[i, 0], 1] - act[intersec[i, 1], 1]);
+            }
+
+            for (int i = intersec.GetLength(0) + kol_S; i < matr.GetLength(1) - 1; i++) //заполняем z-строку для R
+            {
+                matr[0, i] = -M;
+            }
+            // заполняем матрицу единицами
+            int k = 0;
+            for (int i = 0; i < intersec.GetLength(0); i++) //заполняем матрицу единицами
+
+            {
+                //заполняем ограничения по активам и пассивам
+                matr[intersec[i, 0] + 1, i] = 1;
+                matr[intersec[i, 1] + 1 + pas.GetLength(0), i] = 1;
+
+                matr[intersec[i, 0] + 1, intersec.GetLength(0) + kol_S + intersec[i, 0]] = 1;
+                matr[intersec[i, 1] + 1 + pas.GetLength(0), intersec.GetLength(0) + kol_S + pas.GetLength(0) + intersec[i, 1]] = 1;
+
+                matr[intersec[i, 0] + 1, matr.GetLength(1) - 1] = pas[intersec[i, 0], 0];
+                matr[intersec[i, 1] + 1 + pas.GetLength(0), matr.GetLength(1) - 1] = act[intersec[i, 1], 0];
+
+
+                if ((intersec[i, 0] == nom_rez) && (intersec[i, 1] < rezerv)) //заполняем ограничения резервов
+                {
+                    matr[pas.GetLength(0) + act.GetLength(0) + 1 + k, i] = 1;
+                    matr[pas.GetLength(0) + act.GetLength(0) + 1 + k, intersec.GetLength(0) + k] = -1;
+                    matr[pas.GetLength(0) + act.GetLength(0) + 1 + k, intersec.GetLength(0) + kol_S + pas.GetLength(0) + act.GetLength(0) + k] = 1;
+                    matr[pas.GetLength(0) + act.GetLength(0) + 1 + k, matr.GetLength(1) - 1] = act[intersec[i, 1], 0] / sum_rez * pas[nom_rez, 0];
+
+                    k += 1;
+
+                }
+
+            }
+            //убираем М из z-строки
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                for (int i = 1; i < matr.GetLength(0); i++)
+                {
+                    matr[0, j] += M * matr[i, j];
+                }
+            }
+
+            int q = 1;
+
+            while (q != 0) //реализация симплекс-метода
+            {
+
+                double max = 0;
+                double min = 100000000;
+                int bazstolb = 0;
+                int bazstroka = 0;
+                double delim = 0;
+
+                for (int i = 0; i < (matr.GetLength(1) - 1); i++) //находим базовый столбец
+                {
+                    if (matr[0, i] > max)
+                    {
+                        max = matr[0, i];
+                        bazstolb = i;
+                    }
+                }
+
+
+
+
+                for (int i = 1; i < matr.GetLength(0); i++) //находим базовую строку
+                {
+                    if (matr[i, bazstolb] != 0)
+                    {
+                        delim = matr[i, matr.GetLength(1) - 1] / matr[i, bazstolb];
+                        if ((delim < min) && (delim > 0))
+                        {
+                            min = delim;
+                            bazstroka = i;
+                        }
+                    }
+                }
+
+                otvet[bazstroka - 1, 0] = bazstolb; //вывод из базиса небазовой переменной и добавление базовой
+
+                double bazelem;
+                bazelem = matr[bazstroka, bazstolb]; //выделели базовый элемент
+
+                for (int i = 0; i < matr.GetLength(1); i++)
+                {
+                    matr[bazstroka, i] = matr[bazstroka, i] / bazelem; //пересчитываем базовую строку
+                }
+
+                double[] stolb = new double[matr.GetLength(0)];
+
+                for (int i = 0; i < matr.GetLength(0); i++)
+                {
+                    stolb[i] = matr[i, bazstolb];
+                }
+
+                for (int i = 0; i < matr.GetLength(0); i++) //пересчитываем матрицу
+                {
+                    for (int j = 0; j < matr.GetLength(1); j++)
+                    {
+                        if (i != bazstroka)
+                        {
+                            matr[i, j] = matr[i, j] - (stolb[i] * matr[bazstroka, j]);
+
+                        }
+
+                    }
+                }
+
+
+                q = 0; //проверка на оптимальность
+                for (int i = 0; i < matr.GetLength(1) - 1; i++)
+                {
+                    if (matr[0, i] > 0)
+                    {
+                        q += 1;
+
+                    }
+                }
+
+            }
+
+            double[,] itog = new double[intersec.GetLength(0), 2]; //матрица для итоговых переменных
+
+
+            for (int i = 0; i < otvet.GetLength(0); i++)
+            {
+                otvet[i, 1] = matr[i + 1, matr.GetLength(1) - 1];
+            }
+
+            for (int i = 0; i < intersec.GetLength(0); i++)
+            {
+                itog[i, 0] = i;
+                for (int j = 0; j < otvet.GetLength(0); j++)
+                {
+                    if ((otvet[j, 0] == i) && (otvet[j, 1] != 0))
+                    {
+                        itog[i, 1] = otvet[j, 1];
+                    }
+                }
+
+
+            }
+
+            double[,] fond = new double[pas.GetLength(0), act.GetLength(0)];
+
+            for (int i = 0; i < pas.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < act.GetLength(0); j++)
+                {
+                    for (int r = 0; r < intersec.GetLength(0); r++)
+                    {
+                        if ((i == intersec[r, 0]) && (j == intersec[r, 1]))
+                        {
+                            fond[i, j] = Math.Round(itog[r, 1]);
+                        }
+                    }
+                }
+            }
+
+            //вывод финальной таблицы
+
+            int kol_str = fond.GetLength(0);
+            int kol_stolb = fond.GetLength(1);
+
+
+            Form2 endtable = new Form2();
+
+            endtable.Show();
+            endtable.dataGridView1.RowCount = kol_str;
+            endtable.dataGridView1.ColumnCount = kol_stolb;
+
+            for (int i = 0; i < kol_str; ++i)
+                for (int j = 0; j < kol_stolb; ++j)
+                { 
+                    endtable.dataGridView1.Rows[i].Cells[j].Value = fond[i, j];
+                    endtable.dataGridView1.Rows[i].HeaderCell.Value = pas_str[i][3];
+                    endtable.dataGridView1.Columns[i].HeaderCell.Value = act_str[i][3];
+                }
+
+            //    dataGridView1.RowCount = kol_str;
+            //  dataGridView1.ColumnCount = kol_stolb;
+
+            //for (int i = 0; i < kol_str; ++i)
+            //  for (int j = 0; j < kol_stolb; ++j)
+            //    dataGridView1.Rows[i].Cells[j].Value = fond[i, j];
+
         }
     }
 }
